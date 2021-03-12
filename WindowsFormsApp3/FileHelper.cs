77db9f39b062e882e551460ad3ed4211285f98ef
RikoBgrff <riko.bagirli777@gmail.com>
2021-03-12 14:40:48 +0400

    cannot deserialize but everything works

---
+++ WindowsFormsApp3/FileHelper.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp3
+{
+    class FileHelper
+    {
+        public  void JsonSerialize(User person)
+        {
+            string fileName = person.Name + ".json";
+            var serializer = new JsonSerializer();
+            using (var sw = new StreamWriter(fileName))
+            {
+                using (var jw = new JsonTextWriter(sw))
+                {
+                    jw.Formatting = Newtonsoft.Json.Formatting.Indented;
+                    serializer.Serialize(jw, person);
+                }
+            }
+        }
+
+        public User JsonDeserialize(string filename)
+
+        {
+            User person = new User();
+            var serializer = new JsonSerializer();
+            using (var sr = new StreamReader($"{filename}.json"))
+            {
+                using (var jr = new JsonTextReader(sr))
+                {
+                    person = serializer.Deserialize<User>(jr);
+                }
+            }
+            return person;
+        }
+    }
+}
--- WindowsFormsApp3/Form1.cs
+++ WindowsFormsApp3/Form1.cs
@@ -14,13 +14,19 @@ namespace WindowsFormsApp3
 {
     public partial class Form1 : Form
     {
+        FileHelper helper = new FileHelper();
+        bool changeBtnPressed = false;
         public Form1()
         {
+
             InitializeComponent();
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
+            AddBtn.Location = new Point(119, 271);
+            ChangeBtn.Location = new Point(119, 299);
+
             listBox1.Items.Add(
                 new User
                 {
@@ -29,32 +35,11 @@ namespace WindowsFormsApp3
                 );
         }
 
-        class User
-        {
-            public string Name { get; set; }
-            public string Surname { get; set; }
-            public string Email { get; set; }
-            public string Phone { get; set; }
-            public DateTime BirthDate { get; set; }
 
-        }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             listBox1.DisplayMember = "Name";
-        }
-        static void JsonSerialize(User user)
-        {
-            string fileName = user.Name + ".json";
-            var serializer = new JsonSerializer();
-            using (var sw = new StreamWriter(fileName))
-            {
-                using (var jw = new JsonTextWriter(sw))
-                {
-                    jw.Formatting = Newtonsoft.Json.Formatting.Indented;
-                    serializer.Serialize(jw, user);
-                }
-            }
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -68,39 +53,36 @@ namespace WindowsFormsApp3
                 Phone = textBox4.Text,
                 BirthDate = dateTimePicker1.Value
             };
-            JsonSerialize(user);
+            helper.JsonSerialize(user);
         }
-        void JsonDeserialize(User user)
-        {
-            string fileName = textBox5.Text;
-            User[] datas = null;
-            var serializer = new JsonSerializer();
 
-            using (var sr = new StreamReader(fileName))
-            {
-                using (var jr = new JsonTextReader(sr))
-                {
-                    datas = serializer.Deserialize<User[]>(jr);
-                }
-                foreach (var item in datas)
-                {
-                    textBox1.Text = item.Name;
-                    textBox2.Text = item.Surname;
-                    textBox3.Text = item.Email;
-                    textBox4.Text = item.Phone;
-                    dateTimePicker1.Value = item.BirthDate;
-                }
-            }
-
-        }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            User user = new User
+            helper.JsonDeserialize(fileNameTxtBx.Text);
+            var obj = listBox1.SelectedItem as User;
+            var p = helper.JsonDeserialize((obj)?.Name);
+            textBox1.Text = p.Name;
+            textBox2.Text = p.Surname;
+            textBox3.Text = p.Email;
+            textBox4.Text = p.Phone;
+            dateTimePicker1.Value = p.BirthDate;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var obj = listBox1.SelectedItem;
+        }
+
+        private void ChangeBtn_Click(object sender, EventArgs e)
+        {
+            changeBtnPressed = true;
+            if (changeBtnPressed == true)
             {
-                Name = textBox5.Name
-            };
-            JsonDeserialize(user);
+                ChangeBtn.Location = new Point(119, 271);
+                AddBtn.Location = new Point(119, 299);
+            }
         }
     }
 }
+
